21a2ca51d6cf326a344d86e8c75e3b4de74cc14d
Francisco Araujo <francisco@opiagile.com>
2022-07-11 23:52:14 -0300

    Eventos de Customer

--- src/domain/entity/customer.ts
+++ src/domain/entity/customer.ts
@@ -1,3 +1,5 @@
+import EventDispatcherInterface from "../event/@shared/event-dispatcher.interface";
+import CustomerChangedAddressEvent from "../event/customer/customer-changed-address.event";
 import Address from "./address";
 
 export default class Customer {
@@ -6,6 +8,7 @@ export default class Customer {
     private _address!: Address; // opcional
     private _active: boolean = false;
     private _rewardPoints: number = 0;
+    private _eventDispatcher: EventDispatcherInterface;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -37,6 +40,17 @@ export default class Customer {
 
     changeAddress(address: Address) {
         this._address = address;
+        this.notifyCustomerChangedAddress();
+    }
+
+    private notifyCustomerChangedAddress() {
+        const eventData = {
+            id: this._id,
+            name: this._name,
+            address: this._address.toString(),
+        };
+        const costumerChangedAddressEvent = new CustomerChangedAddressEvent(eventData);
+        this._eventDispatcher?.notify(costumerChangedAddressEvent);
     }
 
     get address(): Address {
@@ -68,4 +82,29 @@ export default class Customer {
         return this._rewardPoints;
     }
 
+    set eventDispatcher(dispatcher: EventDispatcherInterface) {
+        this._eventDispatcher = dispatcher;
+    }
 }
+
+// Os dados necessitam estar consistentes
+// Pela regra de negocio não existem clientes sem nome
+// let customer = new Customer("123");
+
+// Dados consistentes
+// devem estar disponiveis em todo o sistema
+// let customer = new Customer("123", "Maria de Fatima");
+
+// Uma entidade por padrão deve ser sempre autovalidada
+// Se ela não se auto-valida há chance de gerar inconsistência
+
+// Entidade vs ORM
+// Esta entidade deste arquivo é focada em negocio.
+// O ORM necessita de uma entidade focada em persistencia (model)
+// Exemplo de estruturacao de arquivos com separacao
+// Domain => complexidade de negocio
+// - entity
+// -- customer.ts (regra de negocio)
+// infrastructure (mundo externo) => complexidade acidental
+// - entity (ou model)
+// -- customer.ts (getters e setters)
